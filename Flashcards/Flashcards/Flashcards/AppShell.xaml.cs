75cae67cc84aec59d06995034f8f1af4a02dc833
lukwyp <wypchlo.lukasz@gmail.com>
2023-04-18 18:57:14 +0200

    Added Flashcards project

---
+++ Flashcards/Flashcards/Flashcards/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Flashcards.ViewModels;
+using Flashcards.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Flashcards
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Flashcards/Flashcards/Flashcards/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Flashcards.Models;
+using Flashcards.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Flashcards.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
