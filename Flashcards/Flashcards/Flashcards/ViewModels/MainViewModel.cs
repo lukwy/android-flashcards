31860b2720bcef2be018fd9ae1b9cb42da6cb5f8
lukwyp <wypchlo.lukasz@gmail.com>
2023-06-29 09:42:18 +0200

    Updated

--- Flashcards/Flashcards/Flashcards/ViewModels/MainViewModel.cs
+++ Flashcards/Flashcards/Flashcards/ViewModels/MainViewModel.cs
@@ -7,17 +7,32 @@ namespace Flashcards.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private const string CorrectAnswer = "Poprawna odpowiedź!";
+        private const string WrongAnswer = "Niepoprawna odpowiedź!";
+
         private string checkStatus;
         private string answer;
         private int itemsCount;
         private Item item;
         private string itemMeaning;
+        private Color statusColor = Color.Transparent;
 
         public MainViewModel()
         {
             Title = "Fiszki";
             CheckCommand = new Command(OnCheck);
+            DrawCommand = new Command(OnDraw);
             InitializeAsync().Wait();
+        }
+
+        public Color StatusColor
+        { 
+            get => statusColor;
+            set
+            { 
+                statusColor = value;
+                OnPropertyChanged(nameof(StatusColor));
+            }
         }
 
         public string CheckStatus
@@ -48,16 +63,25 @@ namespace Flashcards.ViewModels
 
         public Command CheckCommand { get; }
 
+        public Command DrawCommand { get; }
+
         private void OnCheck()
         {
             if (item.Word.Equals(answer, StringComparison.OrdinalIgnoreCase))
             {
-                CheckStatus = "Poprawna odpowiedź!";
+                CheckStatus = $"{CorrectAnswer}{Environment.NewLine}{item.Word}";
+                StatusColor = Color.Green;
             }
             else
             {
-                CheckStatus = "Niepoprawna odpowiedź!";
+                CheckStatus = $"{WrongAnswer}{Environment.NewLine}{item.Word}";
+                StatusColor = Color.Red;
             }
+        }
+
+        private async void OnDraw() 
+        {
+            await GetRandItemAsync();
         }
 
         private async Task LoadRandItemAsync()
@@ -78,6 +102,9 @@ namespace Flashcards.ViewModels
             item = await DataStore.GetRandItemAsync(rnd.Next(itemsCount));
 
             ItemMeaning = item.Meaning;
+            CheckStatus = String.Empty;
+            StatusColor = Color.Transparent;
+            Answer = String.Empty;
         }
     }
 }
